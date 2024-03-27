447d1edde52073d3e5e326b807f3fee252a98607
KimSungKwon <sungkwonn@naver.com>
2024-03-28 00:11:14 +0900

    [design] set body layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+
+import styled from "styled-components";
+
+const BodyInner = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: gray;
+`
+const WebAppContainer = styled.div`
+  width: 100%;
+  max-width: 500px;
+  height: 100%;
+  background-color: white;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BodyInner>
+        <WebAppContainer>
+          Body
+        </WebAppContainer>
+      </BodyInner>
     </div>
   );
 }
