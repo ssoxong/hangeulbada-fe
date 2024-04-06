4afef7ca1c7877daa10c4c595499276e40da2918
KimSungKwon <sungkwonn@naver.com>
2024-04-07 01:02:53 +0900

    [chore] add icon 'button-icon'

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -2,3 +2,4 @@ export { default as Logo1 } from "./logo1.svg";
 export { default as NavbarLogo } from "./navbar-logo.svg";
 export { default as UserIcon } from "./user-icon.svg";
 export { default as BackIcon } from "./back-icon.svg";
+export { default as ButtonIcon } from "./button-icon.svg";
