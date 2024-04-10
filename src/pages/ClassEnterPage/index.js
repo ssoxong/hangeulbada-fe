e0d24a2a32351c526c218360f4e0d379390e6f26
yelim <redheadannielim@gmail.com>
2024-04-10 21:03:58 +0900

    chore: button 적용

--- src/pages/ClassEnterPage/index.js
+++ src/pages/ClassEnterPage/index.js
@@ -1,92 +1,102 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useState } from 'react';
+import ContainedButton from '../../components/button/ContainedButton';
 
 const ClassEnterPageLayout = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  min-height: 100vh;
-  position: relative;
-  //align-items: center;
-  background-color: rgba(74, 190, 255, 0.25);
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    min-height: 100vh;
+    position: relative;
+    //align-items: center;
+    background-color: rgba(74, 190, 255, 0.25);
 `;
 const ClassListHeader = styled.div`
-  display: flex;
-  flex-direction: column;
-  //align-items: center;
-  //justify-content: space-between;
-  margin: 24px;
-`
+    display: flex;
+    flex-direction: column;
+    //align-items: center;
+    //justify-content: space-between;
+    margin: 24px;
+`;
 const ClassListTitle = styled.div`
-  display: flex;
-  font-size: 24px;
-  font-weight: 500;
-`
+    display: flex;
+    font-size: 24px;
+    font-weight: 500;
+`;
 
 const ClassListSubTitle = styled.div`
-display: flex;
-margin: 2% 0;
-`
+    display: flex;
+    margin: 2% 0;
+`;
 
 const StyledForm = styled.form`
-display: flex;
-flex-direction: column;
-`
+    display: flex;
+    flex-direction: column;
+`;
 
 const SyltedInput = styled.input`
-// background-color: yellow;
-border-radius: 20px;
-width: 50%;
-height: 100px;
-padding: 20px;
-font-size: 16px;
-
-
-`
+    // background-color: yellow;
+    border-radius: 20px;
+    border-width: 0px;
+    width: 50%;
+    height: 100px;
+    padding: 20px;
+    font-size: 16px;
+`;
 
 const StyledLabel = styled.label`
-padding: 30px;
-`
+    padding: 30px;
+`;
 
 const StyledDone = styled.button`
-background-color: white;
-width: 150px;
-height: 60px;
-border-radius: 10px;
-border-color: white;
-margin: 0 180px;
-font-size: 20px;
-`
+    background-color: transparent;
+    // width: 150px;
+    // height: 60px;
+    // border-radius: 10px;
+    border-color: transparent;
+    // margin: 0 180px;
+    // font-size: 20px;
+`;
 
 const ClassEnterPage = () => {
-  const [classcode, setClasscode] = useState('');
+    const [inputCheck, setInputCheck] = useState(false);
+    const [classcode, setClasscode] = useState('');
 
-  const handleChange = (e) => {
-    setClasscode(e.target.value);
-  }
+    const handleChange = (e) => {
+        setClasscode(e.target.value);
+    };
 
-  const handleSubmit = (e) => {
-    alert('입력한 코드: ' + classcode);
-    e.preventDefault();
-  }
+    const handleSubmit = (e) => {
+        alert('입력한 코드: ' + classcode);
+        e.preventDefault();
+    };
 
-  return (
+    return (
+        <ClassEnterPageLayout>
+            <ClassListHeader>
+                <ClassListTitle>나의 클래스</ClassListTitle>
+                <ClassListSubTitle>선생님의 클래스 코드를 입력하세요.</ClassListSubTitle>
+            </ClassListHeader>
 
-    <ClassEnterPageLayout>
-        <ClassListHeader>
-          <ClassListTitle>나의 클래스</ClassListTitle>
-          <ClassListSubTitle>선생님의 클래스 코드를 입력하세요.</ClassListSubTitle>
-        </ClassListHeader>
+            <StyledForm onSubmit={handleSubmit}>
+                <StyledLabel>
+                    {' '}
+                    <SyltedInput
+                        type="text"
+                        placeholder="여기에 입력하세요."
+                        name="classcode"
+                        classcode={classcode}
+                        onChange={handleChange}
+                    />
+                </StyledLabel>
+            </StyledForm>
 
-        <StyledForm onSubmit={handleSubmit}>
-          <StyledLabel> <SyltedInput type="text" placeholder="여기에 입력하세요." name="classcode" classcode={classcode} onChange={handleChange}/>
-            
-          </StyledLabel>
-          <StyledDone type="submit" >완료</StyledDone>
-        </StyledForm>
-    </ClassEnterPageLayout>
-  )
-}
+            <StyledDone>
+                <ContainedButton btnType="primary" size="mid" text="완료" />
+            </StyledDone>
+        </ClassEnterPageLayout>
+    );
+};
 
 export default ClassEnterPage;
