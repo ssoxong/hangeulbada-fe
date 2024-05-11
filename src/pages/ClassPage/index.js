a0a015fc6cfa7e09dda82571715ae0e8a234a222
KimSungKwon <sungkwonn@naver.com>
2024-05-12 01:14:20 +0900

    [feature] get class data API

--- src/pages/ClassPage/index.js
+++ src/pages/ClassPage/index.js
@@ -6,138 +6,159 @@ import ContainedButton from '../../components/button/ContainedButton';
 import Dropdown from '../../components/button/Dropdown';
 import CopyButton from './components/CopyButton';
 import { useParams } from 'react-router-dom';
-import { getClass } from '../../utils/api/class';
+import { getClass, getClassSet } from '../../utils/api/class';
 
 const ClassPageLayout = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    min-height: 100vh;
-    position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  min-height: 100vh;
+  position: relative;
 `;
 const ClassHeader = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    margin: 24px;
-    font-family: 'DXSamgakGimbap Medium';
-    font-size: 24px;
-    .title {
-        text-align: start;
-    }
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 24px;
+  font-family: 'DXSamgakGimbap Medium';
+  font-size: 24px;
+  .title {
+      text-align: start;
+  }
 `;
 const ClassTitleBox = styled.div`
-    display: flex;
-    margin-right: 50px;
-    width: 100%;
+  display: flex;
+  margin-right: 50px;
+  width: 100%;
 `;
 const HeaderButtonBox = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 const SetColums = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 8px 16px;
-    margin: 2px 15px;
-    border-radius: 14px;
-    background-color: transparent;
-    font-family: 'DXSamgakGimbap Light';
-    font-size: 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 8px 16px;
+  margin: 2px 15px;
+  border-radius: 14px;
+  background-color: transparent;
+  font-family: 'DXSamgakGimbap Light';
+  font-size: 15px;
 
-    .item-no {
-        flex-basis: 7%;
-    }
-    .item-name {
-        flex-basis: 15%;
-    }
-    .item-set {
-        flex-basis: 20%;
-    }
-    .item-score {
-        white-space: pre-line;
-        flex-basis: 20%;
-    }
-    .item-blank {
-        white-space: pre-line;
-        flex-basis: 5%;
-    }
+  .item-no {
+      flex-basis: 7%;
+  }
+  .item-name {
+      flex-basis: 15%;
+  }
+  .item-set {
+      flex-basis: 20%;
+  }
+  .item-score {
+      white-space: pre-line;
+      flex-basis: 20%;
+  }
+  .item-blank {
+      white-space: pre-line;
+      flex-basis: 5%;
+  }
 `;
 
 const ClassPage = () => {
-    const [classData, setClassData] = useState();
-    const { id } = useParams();
+  const { id } = useParams();
 
-    const getClassData = async () => {
-        await getClass(id)
-            .then((res) => {
-                setClassData(res.data)
-                console.log(classData);
-            })
+  const classInitialState = {
+    groupName: '',
+    groupCode: '',
+    id: '',
+    studentIds: '',
+    teacherId: '',
+  }
+  const setInitialState = [
+    {
+      name: '',
+      workbookId: '',
+      assignmentId: '',
+      score: '',
+      submitDate: '',
     }
+  ]
 
-    useEffect(() => {
-        console.log(id);
-        getClassData();
-    }, [])
+  const [classData, setClassData] = useState(classInitialState);
+  const [classSetData, setClassSetData] = useState(setInitialState)
 
-    const dummies = [
-        {
-            no: '1',
-            name: 'name1',
-            set: 'set1',
-            score: 'score1',
-        },
-        {
-            no: '2',
-            name: 'name2',
-            set: 'set2',
-            score: 'score2',
-        },
-        {
-            no: '3',
-            name: 'name3',
-            set: 'set3',
-            score: 'score3',
-        },
-        {
-            no: '4',
-            name: 'name4',
-            set: 'set4',
-            score: 'score4',
-        },
-    ];
-    const items = [
-        { text: '이름순', link: '#' },
-        { text: '성적순', link: '#' },
-    ];
-    return (
-        <ClassPageLayout>
-            <ClassHeader>
-                <ClassTitleBox>
-                    <div className="title">클래스 1 {/*클래스 명*/}</div>
-                    <CopyButton code={classData.groupCode} />
-                </ClassTitleBox>
-                <HeaderButtonBox>
-                    <ContainedButton btnType="primary" size="large" text="이 클래스 세트 보기" />
+  const getClassData = async () => {
+    await getClass(id)
+      .then((res) => {
+        setClassData(res.data);
+      })
+    await getClassSet(id)
+      .then((res) => {
+        setClassSetData(res.data);
+      })
+  }
 
-                    <Dropdown text="정렬 옵션" items={items} />
-                </HeaderButtonBox>
-            </ClassHeader>
-            <SetColums>
-                <div className="item-no">순</div>
-                <div className="item-name">이름</div>
-                <div className="item-set">마지막으로{'\n'}푼 세트</div>
-                <div className="item-score">마지막{'\n'}세트 점수</div>
-                <div className="item-blank"></div>
-            </SetColums>
-            {dummies.map((dummy) => (
-                <SetCard key={dummy.no} no={dummy.no} name={dummy.name} set={dummy.set} score={dummy.score} />
-            ))}
-        </ClassPageLayout>
-    );
+  useEffect(() => {
+    getClassData();
+  }, []);
+
+  // const dummies = [
+  //   {
+  //     no: '1',
+  //     name: 'name1',
+  //     set: 'set1',
+  //     score: 'score1',
+  //   },
+  //   {
+  //     no: '2',
+  //     name: 'name2',
+  //     set: 'set2',
+  //     score: 'score2',
+  //   },
+  //   {
+  //     no: '3',
+  //     name: 'name3',
+  //     set: 'set3',
+  //     score: 'score3',
+  //   },
+  //   {
+  //     no: '4',
+  //     name: 'name4',
+  //     set: 'set4',
+  //     score: 'score4',
+  //   },
+  // ];
+  const items = [
+    { text: '이름순', link: '#' },
+    { text: '성적순', link: '#' },
+  ];
+  return (
+    <ClassPageLayout>
+      <ClassHeader>
+        <ClassTitleBox>
+          <div className="title">{classData.groupName}</div>
+          <CopyButton code={classData.groupCode} />
+        </ClassTitleBox>
+        <HeaderButtonBox>
+          <ContainedButton btnType="primary" size="large" text="이 클래스 세트 보기" />
+
+          <Dropdown text="정렬 옵션" items={items} />
+        </HeaderButtonBox>
+      </ClassHeader>
+      <SetColums>
+        <div className="item-no">순</div>
+        <div className="item-name">이름</div>
+        <div className="item-set">마지막으로{'\n'}푼 세트</div>
+        <div className="item-score">마지막{'\n'}세트 점수</div>
+        <div className="item-blank"></div>
+      </SetColums>
+      {classSetData.map((data, index) => (
+        <SetCard key={data.index} no={data.index+1} name={data.name} set={data.workbookId} score={data.score} />
+      ))}
+    </ClassPageLayout>
+  );
 };
 
 export default ClassPage;
