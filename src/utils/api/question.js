892725012bf85b06dbe0aa5bc91f5ad58e483580
KimSungKwon <sungkwonn@naver.com>
2024-05-24 01:07:49 +0900

    [feature] set api

--- src/utils/api/question.js
+++ src/utils/api/question.js
@@ -42,3 +42,58 @@ export const getAllQuestions = async () => {
     })
     return returnValue;
 }
+
+export const getSetQuestions = async (workbookId) => {
+  let returnValue;
+
+  await client
+    .get(`/api/v1/workbook/${workbookId}/questions`,
+      {
+        headers: privateHeaders
+      }
+    )
+    .then((res) => {
+      returnValue = res;
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    return returnValue;
+}
+
+export const addQuestion = async (workbookId, content) => {
+  let returnValue;
+
+  await client
+    .post(`/api/v1/workbook/${workbookId}/questions`,
+      {
+        content: content,
+      },
+      {
+        headers: privateHeaders
+      }
+    )
+    .then((res) => {
+      returnValue = res.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    return returnValue;
+}
+
+export const removeQuestion = async (questionId) => {
+  let returnValue;
+
+  await client 
+    .delete(`/api/v1/questions/${questionId}`, {
+      headers: privateHeaders,
+    })
+    .then((res) => {
+      returnValue = res;
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    return returnValue;
+}
